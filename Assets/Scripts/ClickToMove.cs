1e7085fd2762b566d8ef886ccebdb126824ddc0a
dima <aden55@yandex.ru>
2018-01-08 19:36:10 +0300

    ver2

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -17,11 +17,9 @@ public class ClickToMove : MonoBehaviour {
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-
         if (Physics.Raycast(ray, out hit, 500) && Input.GetMouseButton(0))
-        {	
+        {
             nav.SetDestination(hit.point);
-			//GameObject obj=Instantiate(prefab,new Vector3(hit.point.x,hit.point.y,hit.point.z), Quaternion.identity) as GameObject;
         }
     }
 }
---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour {
+
+	public Transform EndPoint;
+	public float speed;
+
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, EndPoint.transform.position, speed);
+	}
+}
